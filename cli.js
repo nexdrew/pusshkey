37ffaae4c3f85c7daef14e8ce77293acecca00b6
Andrew Goode <andrew.goode@nextraq.com>
2014-10-31 01:15:01 -0400

    take 1

---
+++ cli.js
@@ -0,0 +1,81 @@
+#!/usr/bin/env node
+'use strict';
+
+var commander = require('commander');
+var pkg = require('./package.json');
+var pusshkey = require('./');
+
+commander.version(pkg.version);
+
+var cmds = ['add', 'rm'];
+var cmdArgs = ['<key>', '<host>', '[hosts...]'];
+
+var cmdsFull = cmds.map(function(c, i, a) { return c+' '+cmdArgs.join(' '); });
+cmdsFull.push('hosts');
+
+var brkLength = cmdsFull.reduce(function(p, c, i, a) { return a[i].length > p ? a[i].length : p }, 0);
+var brk = '\n'+new Array(brkLength+3).join(' ');
+
+var sharedDesc =
+	brk+cmdArgs[0]+' should be the path to the public key file or the key value itself'
+	+brk+cmdArgs[1]+' should be an IP, hostname, or "hosts" key from '+pusshkey.JSON_FILE_NAME
+	+brk+cmdArgs[2]+' allows you to define additional <host> values';
+
+var config = {};
+
+var handleCommand = function(command, key, host, hosts) {
+	config.execute = true;
+	config.command = command;
+	config.key = key;
+	config.hosts = (hosts || [host]);
+	if(hosts) config.hosts.unshift(host);
+	config.user = commander.user;
+	config.identity = commander.identity;
+	config.parallel = !!commander.parallel;
+	config.force = !!commander.force;
+	// console.log('commander:', commander);
+};
+
+commander
+	.command(cmdsFull[0])
+	.description(
+		'Add key to remote host'
+		+brk+'Will only add the key if it does not already exist'
+		// +' (see --force option)'
+		+sharedDesc
+		+'\n')
+	.action(function add(key, host, hosts) {
+		handleCommand(cmds[0], key, host, hosts);
+	});
+
+commander
+	.command(cmdsFull[1])
+	.description(
+		'Remove key from remote host'
+		+brk+'Will attempt to remove the key if it\'s found'
+		+sharedDesc
+		+'\n')
+	.action(function rm(key, host, hosts) {
+		handleCommand(cmds[1], key, host, hosts);
+	});
+
+commander
+	.command(cmdsFull[2])
+	.description('Output "hosts" object from '+pusshkey.JSON_FILE_NAME)
+	.action(function hosts() {
+		config.execute = false;
+		config.command = cmdsFull[2];
+		console.log((pusshkey.json && pusshkey.json.hosts) || 'No hosts configured. Try adding a '+pusshkey.JSON_FILE_NAME+' file with a "hosts" object.');
+	});
+
+commander.option('-u, --user <user>', 'specify the username to authenticate ssh with - default is '+pusshkey.defaultUser());
+commander.option('-i, --identity <identity_file>', 'specify the identity file (private key) for public key authentication - default is \''+pusshkey.defaultIdentity()+'\'')
+// commander.option('-p, --parallel', 'flag to execute commands against remote hosts concurrently');
+// commander.option('-f, --force', 'flag to forcefully perform the command, whether the key already exists or not');
+
+// parse args and execute command action
+commander.parse(process.argv);
+
+if(!config.command) commander.help();
+
+if(config.execute) pusshkey.execute(config);
---
+++ index.js
@@ -0,0 +1,109 @@
+'use strict';
+
+var fs = require('fs');
+var child_process = require('child_process');
+var ssh = 'ssh';
+
+//==================================================================================
+//-- this logic stolen from https://github.com/mozilla/awsbox/blob/master/lib/ssh.js
+var addSSHPubKey = function(host, pubkey, cb, user, identity) {
+	// Add the key if it is not already in the file
+	var escapedPubkey = pubkey.replace(/(["'`\$\!\*\?\\\(\)\[\]\{\}])/g, '\\$1');
+	var destination = user+'@'+host;
+	var rcmd = 'grep "' + escapedPubkey + '" .ssh/authorized_keys || echo "' + escapedPubkey + '" >> .ssh/authorized_keys';
+	var args = ['-o', 'StrictHostKeyChecking no', destination, rcmd];
+	if(identity) args.unshift('-i', identity);
+	console.log('adding key to '+host);
+	child_process.execFile(ssh, args, cb);
+};
+
+var removeSSHPubKey = function(host, pubkey, cb, user, identity) {
+	// Remove the key from the file
+	// Escape characters that could break sed regex
+	// (nb NOT the + sign)
+	var escapedPubkey = pubkey.replace(/(["'`\$\!\*\?\\\/\(\)\[\]\{\}])/g, '\\$1');
+	var destination = user+'@'+host;
+	var rcmd = 'sed -i "/' + escapedPubkey + '/d" .ssh/authorized_keys';
+	var args = ['-o', 'StrictHostKeyChecking no', destination, rcmd];
+	if(identity) args.unshift('-i', identity);
+	console.log('removing key from '+host);
+	child_process.execFile(ssh, args, cb);
+};
+//==================================================================================
+
+// var tryRequire = function(path, defaultReturn) {
+// 	try { return require(path); } catch(e) { return defaultReturn; }
+// };
+
+var JSON_FILE_NAME = 'pusshkey.json';
+// var json = tryRequire('./'+JSON_FILE_NAME, {});
+var json = require('optional')('./'+JSON_FILE_NAME);
+
+var defaultUser = function() {
+	return (json && json.user) || (process.env['USER'] || 'ec2-user');
+};
+
+var defaultIdentity = function() {
+	return (json && json.identity) || '';
+};
+
+var executeConfig = function(config) {
+	//TODO: validate args!
+	var fx = config.command === 'rm' ? removeSSHPubKey : addSSHPubKey;
+	var pubkey = (fs.existsSync(config.key) ? fs.readFileSync(config.key, { encoding: "utf8" }) : config.key).replace(/(?:\r\n|\r|\n)/g, '');
+	var cb = function(error, stdout, stderr) {
+		//TODO: make this better!
+		if(error) console.log("ERROR!\n"+error);
+		else console.log("success!");
+	};
+
+	//-- expand hosts for any aliases
+	var hosts = [], index = '', i;
+	var expand = function(host) {
+		if(json && json.hosts && json.hosts[host]) {
+			// console.log(host+' is alias, expanding');
+			json.hosts[host].forEach(expand);
+		} else {
+			i = '<'+host+'>';
+			if(index.indexOf(i) == -1) {
+				index += i;
+				hosts.push(host);
+			}
+		}
+	};
+	(config.hosts || []).forEach(expand);
+
+	hosts.forEach(function perHost(host) {
+		// console.log('flattened host: '+host);
+		fx(host, pubkey, cb, config.user || defaultUser(), config.identity || defaultIdentity());
+	});
+};
+
+var execute = function(command, key, hosts, opts) {
+	if(command && command.command && command.key && command.hosts) executeConfig(command);
+	else {
+		var config = opts || {};
+		config.command = command || config.command;
+		config.key = key || config.key;
+		config.hosts = hosts || config.hosts;
+		executeConfig(config);
+	}
+};
+
+var add = function(key, hosts, opts) {
+	//TODO: check first arg
+	execute('add', key, hosts, opts);
+};
+
+var rm = function(key, hosts, opts) {
+	//TODO: check first arg
+	execute('rm', key, hosts, opts);
+};
+
+exports.JSON_FILE_NAME = JSON_FILE_NAME;
+exports.json = json;
+exports.defaultUser = defaultUser;
+exports.defaultIdentity = defaultIdentity;
+exports.execute = execute;
+exports.add = add;
+exports.rm = rm;
